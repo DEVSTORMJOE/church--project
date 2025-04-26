2d0422ac0ed2fb507f1e63206579cf1bf3ab8bd0
devstormjoe <classicnetpro5@gmail.com>
2025-04-26 16:22:19 +0300

    Church system start..

---
+++ church-sys/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+// src/App.js
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-center text-blue-500">
+        Hello, Tailwind CSS in React!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ church-sys/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Update the paths to scan for JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
